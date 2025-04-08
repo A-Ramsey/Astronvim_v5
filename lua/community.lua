35913c8b9983e0d3bcca927773a6ae7dba76407c
Aaron Ramsey <aaronramsey2000@gmail.com>
2025-04-08 22:29:00 +0100

    chore: add plugins from astrocommunity and move some plugin into community ones

--- lua/community.lua
+++ lua/community.lua
@@ -8,5 +8,9 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.workflow.hardtime-nvim" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+  { import = "astrocommunity.split-and-window.colorful-winsep-nvim" },
+  { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
+  { import = "astrocommunity.recipes.auto-session-restore" },
   -- import/override with your plugins folder
 }
